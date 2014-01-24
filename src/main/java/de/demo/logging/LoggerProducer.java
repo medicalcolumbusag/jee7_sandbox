00c78c28d2a8422fc1d259dba0926777b680f080
lauraliparulo <laura.liparulo@medicalcolumbus.de>
2014-01-24 15:46:42 +0100

    Logger injection (including Arquillian test)

---
+++ src/main/java/de/demo/logging/LoggerProducer.java
@@ -0,0 +1,25 @@
+package de.demo.logging;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Logging producer for injectable log4j logger
+ *
+ * @author cem ikta
+ */
+
+public class LoggerProducer {
+   /**
+    * @param injectionPoint
+    * @return logger
+    */
+	  @Produces
+	  public Logger producer(InjectionPoint ip){
+	    return LoggerFactory.getLogger(
+	      ip.getMember().getDeclaringClass().getName());
+	  }
+}
---
+++ src/test/java/de/medicalcolumbus/sandbox/service/InjectedLoggingTest.java
@@ -0,0 +1,35 @@
+package de.medicalcolumbus.sandbox.service;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+
+
+@RunWith(Arquillian.class)
+public class InjectedLoggingTest {
+
+    @Inject
+  Logger log;
+
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+        		.addPackage( "de.demo.logging")
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void should_inject_logging() {
+
+    	  log.debug("INJECTED LOGGER WORKS!!!");
+    }
+
+}
